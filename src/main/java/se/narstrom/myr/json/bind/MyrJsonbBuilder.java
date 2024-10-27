d28e9b9069e98435c61a92f2afe7f89582a29143
Rickard Närström <rickard@narstrom.se>
2024-10-27 18:42:24 +0100

    Initial commit
    Just some basic stubs and a pom.xml file

---
+++ src/main/java/se/narstrom/myr/json/bind/MyrJsonbBuilder.java
@@ -0,0 +1,28 @@
+package se.narstrom.myr.json.bind;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
+import jakarta.json.spi.JsonProvider;
+
+public final class MyrJsonbBuilder implements JsonbBuilder {
+
+	@Override
+	public JsonbBuilder withConfig(final JsonbConfig config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JsonbBuilder withProvider(final JsonProvider jsonpProvider) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Jsonb build() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/se/narstrom/myr/json/bind/MyrJsonbProvider.java
@@ -0,0 +1,13 @@
+package se.narstrom.myr.json.bind;
+
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.spi.JsonbProvider;
+
+public final class MyrJsonbProvider extends JsonbProvider {
+
+	@Override
+	public JsonbBuilder create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
