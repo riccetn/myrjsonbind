8381d10a43486c3de6156e63309a519142858e88
Rickard Närström <rickard@narstrom.se>
2024-10-30 19:57:57 +0100

    Cannot use a super class constructor

--- src/main/java/se/narstrom/myr/json/bind/DefaultDeserializer.java
+++ src/main/java/se/narstrom/myr/json/bind/DefaultDeserializer.java
@@ -1,6 +1,7 @@
 package se.narstrom.myr.json.bind;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -21,55 +22,135 @@ public final class DefaultDeserializer implements JsonbDeserializer<Object> {
 	public Object deserialize(final JsonParser parser, final DeserializationContext ctx, final Type type) {
 		final Class<?> clazz = (Class<?>) type;
 
-		if (clazz.isArray())
+		if (clazz.isArray()) {
+			if (parser.currentEvent() != Event.START_ARRAY)
+				throw new JsonbException("Not an array: " + parser.currentEvent());
+
 			return deserializeArray(parser, ctx, clazz);
+		}
 
 		if (parser.currentEvent() != Event.START_OBJECT)
 			throw new JsonbException("Not an object");
 
+		return deserializeObject(parser, ctx, clazz);
+	}
+
+	private Object deserializeObject(final JsonParser parser, final DeserializationContext ctx, final Class<?> clazz) {
+
+		final Constructor<?> constructor = findConstructor(clazz);
+		if (constructor == null)
+			throw new JsonbException("No public or protected no-args constructor");
+
 		final Object instance;
 		try {
-			instance = clazz.getConstructor().newInstance();
+			constructor.setAccessible(true);
+			instance = constructor.newInstance();
 		} catch (final ReflectiveOperationException ex) {
 			throw new JsonbException(ex.getMessage(), ex);
+		} finally {
+			constructor.setAccessible(false);
 		}
 
 		Event event;
 		while ((event = parser.next()) != Event.END_OBJECT) {
 			assert event == Event.KEY_NAME;
-			final String key = parser.getString();
+			final String name = parser.getString();
 
-			if (key.isEmpty())
+			if (name.isEmpty()) {
+				parser.getValue();
+				parser.next();
 				continue;
+			}
 
-			final Method setter = findSetter(clazz, key);
+			Field field;
+			try {
+				field = clazz.getField(name);
+			} catch (final NoSuchFieldException ex) {
+				field = null;
+			}
+
+			if (field != null) {
+				if (Modifier.isStatic(field.getModifiers())) {
+					parser.getValue();
+					parser.next();
+					continue;
+				}
+
+				if (Modifier.isFinal(field.getModifiers())) {
+					parser.getValue();
+					parser.next();
+					continue;
+				}
+
+				if (Modifier.isTransient(field.getModifiers())) {
+					parser.getValue();
+					parser.next();
+					continue;
+				}
+			}
+
+			Type propertyType = null;
+			Class<?> propertyClazz = null;
+			if (field != null) {
+				propertyType = field.getGenericType();
+				propertyClazz = field.getType();
+			}
+
+			final Method setter;
+			if (propertyType != null) {
+				setter = getSetter(clazz, name, propertyClazz);
+
+				if (setter != null) {
+					if (!Modifier.isPublic(setter.getModifiers())) {
+						parser.getValue();
+						parser.next();
+						continue;
+					}
+
+					if (Modifier.isStatic(setter.getModifiers())) {
+						parser.getValue();
+						parser.next();
+						continue;
+					}
+
+					if (setter.isBridge()) {
+						parser.getValue();
+						parser.next();
+						continue;
+					}
+				}
+			} else {
+				setter = findSetter(clazz, name);
+
+				if (setter != null) {
+					propertyType = setter.getGenericParameterTypes()[0];
+					propertyClazz = setter.getParameterTypes()[0];
+				}
+			}
+
+			if (propertyType == null) {
+				parser.getValue();
+				parser.next();
+				continue;
+			}
+
+			final Object value = ctx.deserialize(propertyType, parser);
+
 			if (setter != null) {
-				final Class<?> propertyType = setter.getParameterTypes()[0];
-				final Object value = ctx.deserialize(propertyType, parser);
 				try {
 					setter.invoke(instance, value);
 				} catch (final ReflectiveOperationException ex) {
 					throw new JsonbException(ex.getMessage(), ex);
 				}
 			} else {
-				Field field;
+				assert field != null;
 				try {
-					field = clazz.getField(key);
-				} catch (final NoSuchFieldException ex) {
-					field = null;
-				}
-
-				if (field != null && !Modifier.isStatic(field.getModifiers())) {
-					final Class<?> propertyType = field.getType();
-					final Object value = ctx.deserialize(propertyType, parser);
-					try {
-						field.set(instance, value);
-					} catch (final ReflectiveOperationException ex) {
-						throw new JsonbException(ex.getMessage(), ex);
-					}
-				} else {
-					parser.getValue();
-					parser.next();
+					field.setAccessible(true);
+					field.set(instance, value);
+				} catch (final ReflectiveOperationException ex) {
+					throw new JsonbException(ex.getMessage(), ex);
+				} finally {
+					field.setAccessible(false);
 				}
 			}
 		}
@@ -77,15 +158,75 @@ public final class DefaultDeserializer implements JsonbDeserializer<Object> {
 		return instance;
 	}
 
+	private Constructor<?> findConstructor(final Class<?> clazz) {
+		for (final Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+
+			if (!Modifier.isPublic(constructor.getModifiers()) && !Modifier.isProtected(constructor.getModifiers()))
+				continue;
+
+			if (constructor.getParameterCount() != 0)
+				continue;
+
+			return constructor;
+		}
+		return null;
+	}
+
+	private Method getSetter(final Class<?> clazz, final String name, final Class<?> propertyClazz) {
+		final int firstCodePoint = name.codePointAt(0);
+		final String setterName;
+
+		if (firstCodePoint > 0xFFFF)
+			setterName = "set" + Character.toString(Character.toUpperCase(firstCodePoint)) + name.substring(2);
+		else
+			setterName = "set" + Character.toString(Character.toUpperCase(firstCodePoint)) + name.substring(1);
+
+		try {
+			return clazz.getMethod(setterName, propertyClazz);
+		} catch (final NoSuchMethodException ex) {
+			return null;
+		}
+	}
+
+	private Method findSetter(final Class<?> clazz, final String name) {
+		final int firstCodePoint = name.codePointAt(0);
+		final String setterName;
+
+		if (firstCodePoint > 0xFFFF)
+			setterName = "set" + Character.toString(Character.toUpperCase(firstCodePoint)) + name.substring(2);
+		else
+			setterName = "set" + Character.toString(Character.toUpperCase(firstCodePoint)) + name.substring(1);
+
+		for (Class<?> currentClazz = clazz; currentClazz != null && currentClazz != Object.class; currentClazz = currentClazz.getSuperclass()) {
+			for (final Method method : currentClazz.getMethods()) {
+
+				if (!Modifier.isPublic(method.getModifiers()))
+					continue;
+
+				if (Modifier.isStatic(method.getModifiers()))
+					continue;
+
+				if (method.isBridge())
+					continue;
+
+				if (method.getParameterCount() != 1)
+					continue;
+
+				if (!Objects.equals(method.getName(), setterName))
+					continue;
+
+				return method;
+			}
+		}
+
+		return null;
+	}
+
 	private Object deserializeArray(final JsonParser parser, final DeserializationContext ctx, final Class<?> clazz) {
 		final Class<?> componentClazz = clazz.getComponentType();
 		final List<Object> elements = new ArrayList<>();
 
-		if (parser.currentEvent() != Event.START_ARRAY)
-			throw new JsonbException("Not an array: " + parser.currentEvent());
-
-		Event event;
-		while ((event = parser.next()) != Event.END_ARRAY) {
+		while (parser.next() != Event.END_ARRAY) {
 			final Object elem = ctx.deserialize(componentClazz, parser);
 			elements.add(elem);
 		}
@@ -157,34 +298,5 @@ public final class DefaultDeserializer implements JsonbDeserializer<Object> {
 		} else {
 			throw new AssertionError("Unreachable");
 		}
-	}
-
-	private Method findSetter(final Class<?> clazz, final String key) {
-		final int firstCodePoint = key.codePointAt(0);
-		if (!Character.isJavaIdentifierStart(firstCodePoint))
-			return null;
-
-		final String setterName = "set" + Character.toString(Character.toUpperCase(firstCodePoint)) + key.substring(1);
-
-		for (Class<?> currentClazz = clazz; currentClazz != null && currentClazz != Object.class; currentClazz = currentClazz.getSuperclass()) {
-			for (final Method method : currentClazz.getMethods()) {
-
-				if (!Modifier.isPublic(method.getModifiers()))
-					continue;
-
-				if (method.isBridge())
-					continue;
-
-				if (!Objects.equals(method.getName(), setterName))
-					continue;
-
-				if (method.getParameterCount() != 1)
-					continue;
-
-				return method;
-			}
-		}
-
-		return null;
 	}
 }
